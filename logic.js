b094ecb4c6d6f30a4972ad317e1d14d1553660af
IsraaMoqbel <isramm94@gmail.com>
2018-03-20 15:27:15 +0200

    gitignor commit

--- logic.js
+++ logic.js
@@ -2,32 +2,45 @@
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
 
+
+
 var todoFunctions = {
     // todoFunctions.generateId() will give you a unique id
     // You do not need to understand the implementation of this function.
     generateId: (function() {
       var idCounter = 0;
-  
+
       function incrementCounter() {
         return (idCounter += 1);
       }
-  
+
       return incrementCounter;
     })(),
-    
-    //cloneArrayOfObjects will create a copy of the todos array 
+
+    //cloneArrayOfObjects will create a copy of the todos array
     //changes to the new array don't affect the original
     cloneArrayOfObjects: function(todos) {
       return todos.map(function(todo){
         return JSON.parse(JSON.stringify(todo));
       });
     },
-    
+
     addTodo: function(todos, newTodo) {
       // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
       // returns a new array, it should contain todos with the newTodo added to the end.
       // add an id to the newTodo. You can use the generateId function to create an id.
       // hint: array.concat
+      if(newTodo.description.trim().length > 0)
+      {
+
+      var arr =  JSON.parse(JSON.stringify(todos));
+      var obj = {id : todoFunctions.generateId(),
+                description:newTodo.description,
+                done: false,
+                sortId: 0}
+      arr.push(obj);
+    }
+      return arr;
     },
     deleteTodo: function(todos, idToDelete) {
       // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
@@ -47,11 +60,14 @@ var todoFunctions = {
       // hint: array.slice, array.sort
     },
   };
-  
-  
+  //
+  // var todos = ['make smoothie '];
+  // var newTodo = { description: 'make smoothie out of things that should really be cooked' };
+  // var updatedTodos = todoFunctions.addTodo(todos, newTodo);
+  // console.log(updatedTodos)
   // Why is this if statement necessary?
   // The answer has something to do with needing to run code both in the browser and in Node.js
-  // See this article for more details: 
+  // See this article for more details:
   // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
   if (typeof module !== 'undefined') {
     module.exports = todoFunctions;
--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 var test = require('tape');
-var logic = require('./logic');
-
+var todoFunctions = require('./logic.js');
+var todos = [];
+var newTodo = { description: 'make smoothie out of things that should really be cooked' };
+/*var todos = ['make smoothie '];
+var newTodo = { description: 'make smoothie out of things that should really be cooked' };
+var updatedTodos = todoFunctions.addTodo(todos, newTodos);
+console.log(updatedTodos);*/
+var exp=[{id: 1 ,description: "make smoothie out of things that should really be cooked",
+ done: false, sortId:0}]
 test('Example test', function(t) {
-  t.pass();
+  t.deepEqual(todoFunctions.addTodo(todos, newTodo),exp,"it should return make smothie");
   t.end();
 });
